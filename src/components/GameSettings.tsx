4c5104c36ec29d895ba6949f63d0882b9bc3ab22
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-10-12 10:25:49 +0200

    refactor: Gamesettings
    Added gameSettings arrayObject to easy add new difficulties and easier readability

--- src/components/GameSettings.tsx
+++ src/components/GameSettings.tsx
@@ -8,15 +8,24 @@ type GameSettingsProps = {
 const GameSettings: React.FC<GameSettingsProps> = ({ onClick }) => {
   // Render the game settings buttons
   // first value = boardSize, second value = mineCount
-  // returns values to parent component
+
+  // gameSettinsg arrayObject, add to the array the boardSize, mineCount and buttonText if you want to add a new difficulty
+  const gameSettings = [
+    { boardSize: 4, mineCount: 1, buttonText: 'Easy' },
+    { boardSize: 16, mineCount: 4, buttonText: 'Medium' },
+    { boardSize: 25, mineCount: 7, buttonText: 'Hard' },
+    { boardSize: 49, mineCount: 15, buttonText: 'Extreme' },
+    { boardSize: 64, mineCount: 20, buttonText: 'Extreme++' },
+  ];
+
   return (
     <div className='gamesettings-container'>
-      <h1>Choose Difficulty !</h1>
-      <button onClick={() => onClick(4, 1)}>Easy</button>
-      <button onClick={() => onClick(16, 4)}>Medium</button>
-      <button onClick={() => onClick(25, 7)}>Hard</button>
-      <button onClick={() => onClick(49, 15)}>Extreme</button>
-      <button onClick={() => onClick(64, 20)}>Extreme++</button>
+      <h1>Choose Difficulty!</h1>
+      {gameSettings.map((setting, index) => (
+        <button key={index} onClick={() => onClick(setting.boardSize, setting.mineCount)}>
+          {setting.buttonText}
+        </button>
+      ))}
     </div>
   );
 };
